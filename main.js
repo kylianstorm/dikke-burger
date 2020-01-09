40a32d856d8b1ff9968a42be765c0487057c46a1
kylianstorm <54976906+kylianstorm@users.noreply.github.com>
2020-01-09 11:27:45 +0100

    Put all burger ingredients into a group

--- main.js
+++ main.js
@@ -1,70 +1,74 @@
 // create illo
 const illo = new Zdog.Illustration({
   // set canvas with selector
-  element: '.burger-canvas',
-  dragRotate: true,
+  element: '.food-canvas',
   rotate: { x: 5.9, y: 0.85 }
+});
+
+// burger group
+const burgerGroup = new Zdog.Group({
+  addTo: illo,
+  translate: { y: 4 }
 });
 
 // bottom bun
 const bottomBun = new Zdog.Cylinder({
-  addTo: illo,
+  addTo: burgerGroup,
   diameter: 70,
   stroke: 25,
   fill: true,
   color: '#F2D38D',
-  translate: { y: 23 },
   rotate: { x: Zdog.TAU/4 }
 });
 
 // lettuce
 const lettuce = new Zdog.Ellipse({
-  addTo: illo,
+  addTo: burgerGroup,
   height: 60,
   width: 100,
   stroke: 5,
   fill: true,
   color: '#3F9C1F',
-  translate: { y: 7 },
+  translate: { y: -16 },
   rotate: { x: Zdog.TAU/4 }
 });
 
 // patty
 const patty = new Zdog.Ellipse({
-  addTo: illo,
+  addTo: burgerGroup,
   diameter: 90,
   stroke: 12,
   fill: true,
   color: '#592002',
-  translate: { y: -2 },
+  translate: { y: -25 },
   rotate: { x: Zdog.TAU/4 }
 });
 
 // cheese
 const cheese = new Zdog.Rect({
-  addTo: illo,
+  addTo: burgerGroup,
   width: 85,
   height: 85,
   stroke: 5,
   color: '#F2B705',
   fill: true,
-  translate: { y: -11 },
+  translate: { y: -34 },
   rotate: { x: Zdog.TAU/4 }
 });
 
 // top bun
 const topBun = new Zdog.Hemisphere({
-  addTo: illo,
+  addTo: burgerGroup,
   diameter: 90,
   stroke: false,
   color: '#F2D38D',
   backface: '#f8e9c6',
-  translate: { y: -14 },
+  translate: { y: -37 },
   rotate: { x: Zdog.TAU/4 }
 });
 
+
 // // update & render
-// illo.updateRenderGraph();
 const animationObject = {
   topBunTranslateY: -14,
   cheeseTranslateY: -11,
@@ -76,9 +80,9 @@ const animationObject = {
 // GSAP animation
 
 // on hover
-const burgerCanvas = document.querySelector('.burger-canvas');
+const foodCanvas = document.querySelector('.food-canvas');
 
-burgerCanvas.addEventListener("mouseenter", function() {
+foodCanvas.addEventListener("mouseenter", function() {
   const tl = gsap.timeline({onUpdate: render});
   tl
     .to(animationObject, 1, {
@@ -92,7 +96,7 @@ burgerCanvas.addEventListener("mouseenter", function() {
 });
 
 // on mouse out
-burgerCanvas.addEventListener("mouseout", function() {
+foodCanvas.addEventListener("mouseout", function() {
   const tl = gsap.timeline({onUpdate: render});
   tl
     .to(animationObject, 1, {
@@ -106,22 +110,22 @@ burgerCanvas.addEventListener("mouseout", function() {
 });
 
 
-const rotateBurgerObject = {
-  illoRotateY: 0.85
+const rotateGroupObject = {
+  groupRotateY: 0.85
 }
 
-function rotateBurger() {
+function rotateGroup() {
 const rotateTl = gsap.timeline({onUpdate: render, repeat: -1});
 
 rotateTl
-.fromTo(rotateBurgerObject, 4, {
-  illoRotateY: 0.85
+.fromTo(rotateGroupObject, 4, {
+  groupRotateY: 0.85
 }, {
-  illoRotateY: 7.13318531,
+  groupRotateY: 7.13318531,
   ease: "none"
 });
 }
-rotateBurger();
+rotateGroup();
 
 // Render the illustration
 function render() {
@@ -130,7 +134,8 @@ function render() {
   patty.translate.y = animationObject.pattyTranslateY;
   lettuce.translate.y = animationObject.lettuceTranslateY;
   bottomBun.translate.y = animationObject.bottomBunTranslateY;
-  illo.rotate.y = rotateBurgerObject.illoRotateY;
+  burgerGroup.rotate.y = rotateGroupObject.groupRotateY;
+  // friesGroup.rotate.y = rotateGroupObject.groupRotateY;
 
   illo.updateRenderGraph();
 }
