d52109ad2b1b3239f12770c1b5b748033ca8e13e
kylianstorm <54976906+kylianstorm@users.noreply.github.com>
2020-01-09 14:34:25 +0100

    Added fries in a container

--- main.js
+++ main.js
@@ -68,6 +68,91 @@ const topBun = new Zdog.Hemisphere({
 });
 
 
+// fries container
+const friesContainer = new Zdog.Box({
+  addTo: illo,
+  width: 60,
+    height: 50,
+    depth: 20,
+   color: '#F20707',
+    stroke: 5,
+    rotate: { x: 0 },
+    translate: { x: 100 }
+});
+
+// anchor for fries
+let friesAnchor = new Zdog.Anchor({
+  addTo: friesContainer,
+  translate: { y: -50 }
+});
+
+// fry A
+const fryAX = -28;
+const fryAY = 8;
+const fryAZ = -8;
+const fryA = new Zdog.Box({
+  addTo: friesAnchor,
+  width: 3,
+    height: 34,
+    depth: 3,
+    stroke: false,
+    color: '#F2E205',
+    fill: true,
+    stroke: 2,
+    translate: { x: fryAX, y: fryAY, z: fryAZ  },
+});
+
+// fry B
+const fryBX = -28;
+const fryBY = 4;
+const fryBZ = 0;
+const fryB = new Zdog.Box({
+  addTo: friesAnchor,
+  width: 3,
+    height: 42,
+    depth: 3,
+    stroke: false,
+    color: '#F2E205',
+    fill: true,
+    stroke: 2,
+    translate: { x: fryBX, y: fryBY, z: fryBZ },
+});
+
+// fry C
+const fryCX = -28;
+const fryCY = 0;
+const fryCZ = 8;
+const fryC = new Zdog.Box({
+  addTo: friesAnchor,
+  width: 3,
+    height: 50,
+    depth: 3,
+    stroke: false,
+    color: '#F2E205',
+    fill: true,
+    stroke: 2,
+    translate: { x: fryCX, y: fryCY, z: fryCZ },
+});
+
+// copy fry horizontal line
+function copyFries(fryRow, fryX, fryY, fryZ) {
+  const frySpacing = 8;
+  const amountOfFries = 8;
+
+  for (i = 0; i < amountOfFries - 1; i++){
+fryX = fryX + frySpacing;
+
+      fryRow.copy({
+    translate: { x: fryX, y: fryY, z:  fryZ },
+  });
+
+}
+}
+
+copyFries(fryA, fryAX, fryAY, fryAZ);
+copyFries(fryB, fryBX, fryBY, fryBZ);
+copyFries(fryC, fryCX, fryCY, fryCZ);
+
 // // update & render
 const animationObject = {
   topBunTranslateY: -14,
@@ -135,7 +220,7 @@ function render() {
   lettuce.translate.y = animationObject.lettuceTranslateY;
   bottomBun.translate.y = animationObject.bottomBunTranslateY;
   burgerGroup.rotate.y = rotateGroupObject.groupRotateY;
-  // friesGroup.rotate.y = rotateGroupObject.groupRotateY;
+  friesContainer.rotate.y = rotateGroupObject.groupRotateY;
 
   illo.updateRenderGraph();
 }
